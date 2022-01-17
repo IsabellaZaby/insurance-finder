84ef5b6cac66616643fffa56e2d782885ec15cec
Isabella Zaby <i.zaby@netconomy.net>
2022-01-17 14:53:01 +0100

    added email /phone validation

--- src/components/StepFour.tsx
+++ src/components/StepFour.tsx
@@ -4,12 +4,13 @@ import styles from './StepFour.module.css';
 import {useNavigate} from "react-router-dom";
 import {Button, Checkbox, FormControl, FormControlLabel, FormHelperText, Radio, RadioGroup, TextField} from "@mui/material";
 
-
-//TODO fix checkbox and email phone validation
+//TODO fix checkbox
 const StepFour: FC = () => {
 
     const navigate = useNavigate();
     const [errors, setErrors] = useState(false);
+    const [mailValidationError, setMailValidationError] = useState(false);
+    const [phoneValidationError, setPhoneValidationError] = useState(false);
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [phone, setPhone] = useState('');
@@ -18,6 +19,19 @@ const StepFour: FC = () => {
 
     const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, setMethod: (value: string) => void) => {
         setMethod(event.target.value);
+    };
+
+    const handleEmailChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setMail(event.target.value);
+        const regex = new RegExp(
+            "/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;");
+        setMailValidationError(!regex.test(event.target.value));
+    };
+
+    const handlePhoneChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setPhone(event.target.value);
+        const regex = new RegExp("^[0-9]*");
+        setPhoneValidationError(!regex.test(event.target.value));
     };
 
     const onButtonClick = () => {
@@ -74,21 +88,23 @@ const StepFour: FC = () => {
                     id="outlined-basic"
                     label="Email"
                     variant="outlined"
-                    onChange={e => handleChange(e, setMail)}
+                    onChange={handleEmailChange}
                     value={mail}
-                    error={errors && (mail === '' && phone === '')}
+                    error={errors && (mail === '' && phone === '') || mailValidationError}
                 />
+                {mailValidationError && <FormHelperText>Keine gültige Email-Adresse.</FormHelperText>}
                 <h4>UND / ODER</h4>
                 <TextField
                     className={styles.formelement}
                     id="outlined-basic"
                     label="Telefonnummer"
                     variant="outlined"
-                    onChange={e => handleChange(e, setPhone)}
+                    onChange={handlePhoneChange}
                     value={phone}
-                    error={errors && (mail === '' && phone === '')}
+                    error={errors && (mail === '' && phone === '') || phoneValidationError}
                 />
                 {errors && (mail === '' && phone === '') && <FormHelperText>Eines dieser Felder ist Pflicht.</FormHelperText>}
+                {phoneValidationError && <FormHelperText>Die Telefonnummer darf nur aus Zahlen bestehen.</FormHelperText>}
             </FormControl>
             <p>Wir kontaktieren Sie gerne so schnell wie möglich mit mehr Informationen über ihre gewählte Versicherung.</p>
             <div>
@@ -96,9 +112,12 @@ const StepFour: FC = () => {
                     value={checkbox}
                     aria-label={"datenschutz"}
                     onChange={(e) => setCheckBox(!checkbox)}
-                    required={true}/>
+                    required={true}
+                    sx={{color: 'red'}}
+                />
                 Ich stimme den <a>Datenschutzrichtlinien</a> zu.
-                {errors && !checkbox && <FormHelperText className={styles.error}>Sie müssen den Datenschutzrichtlinien zustimmen.</FormHelperText>}
+                {errors && !checkbox && <FormHelperText className={styles.error}>Sie müssen den Datenschutzrichtlinien
+                    zustimmen.</FormHelperText>}
             </div>
             <Button
                 size={"large"}
